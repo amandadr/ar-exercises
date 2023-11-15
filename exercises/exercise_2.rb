2fdec5e012ecb19bf40cd021773c0f1365a095c5
amandadr <amandadroy@gmail.com>
2023-11-14 20:09:43 -0400

    complete exercise_2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,9 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+# Your code goes here ... 
+@store1 = Store.first
+@store2 = Store.find_by(id: 2)
+
+@store1.name = "Not-Burnaby"
+@store1.save
